e87b6d338eef3699cc3918a05c0d089dcc842a55
ItisEternity <lavrovslava207@gmail.com>
2022-11-19 13:31:01 +0300

    Users page layout

---
+++ src/components/UserCard/UserCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Avatar, Button } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import styles from './UserCard.module.scss'
+
+const UserCard = ({photoUrl, name, phoneNumber, requisites, actives, statistics, id}) => {
+  return (
+    <div className={styles.container}>
+      <Avatar className={styles.avatar} src={photoUrl} size={184} shape="square" icon={<UserOutlined />} />
+      <div>
+        <p>{name}</p>
+        <p>{phoneNumber}</p>
+        <p>{requisites}</p>
+        <p>{actives}</p>
+        <p>{statistics}</p>
+        <p>{id}</p>
+        <Button className={styles.blockButton} danger>Заблокировать</Button>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
---
+++ src/mockData.js
@@ -0,0 +1,34 @@
+export const USERS = [
+  {
+    name: 'Маслова Вероника Витальевна',
+    number: '+7 966 430 8962',
+    requisites: '-',
+    actives: '-',
+    statistics: '-',
+    id: '1',
+  },
+  {
+    name: 'Маслова Вероника Витальевна',
+    number: '+7 966 430 8962',
+    requisites: '-',
+    actives: '-',
+    statistics: '-',
+    id: '2',
+  },
+  {
+    name: 'Маслова Вероника Витальевна',
+    number: '+7 966 430 8962',
+    requisites: '-',
+    actives: '-',
+    statistics: '-',
+    id: '3',
+  },
+  {
+    name: 'Маслова Вероника Витальевна',
+    number: '+7 966 430 8962',
+    requisites: '-',
+    actives: '-',
+    statistics: '-',
+    id: '4',
+  },
+]
